7ba4568b39284c2aa4b1f86e560bf07f3ba538ef
Evseredyn <zhenyavn@gmail.com>
2026-01-06 17:18:19 +0200

    coded section steps for mobile

---
+++ js/slider-steps.js
@@ -0,0 +1,16 @@
+stepsSlider = new Swiper('.steps__container', {
+    slidesPerView: 'auto', 
+    spaceBetween: 12, 
+    centeredSlides: false,
+    
+    // Відступ першої картки від лівого краю екрана (ваші 16px)
+    // slidesOffsetBefore: 16, 
+    
+    // Відступ після останньої картки (теж 16px, щоб було симетрично при кінці скролу)
+    // slidesOffsetAfter: 16,  
+    
+    grabCursor: true,
+    resistanceRatio: 0.5,
+    observer: true,
+    observeParents: true
+});
